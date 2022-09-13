3115a1bf5f6d867075c7321f100a12092a5a5997
Harsh Jain <harsh.jain@techture.global>
2022-09-13 06:17:32 +0530

    added all workerfiles

---
+++ script.js
@@ -0,0 +1,25 @@
+const worker = new Worker("worker.js");
+
+const sumButton = document.querySelector("#sumButton");
+const bgButton = document.querySelector("#bgButton");
+
+console.log(sumButton);
+console.log(bgButton);
+
+sumButton.addEventListener("click", (event) => {
+  worker.postMessage("hello");
+});
+
+worker.onmessage = function(message) {
+  console.log(message);
+  alert(`The Sum of i is ${message.data}`);
+}
+
+bgButton.addEventListener("click" , (event) => {
+  if(document.body.style.background !== "green") {
+    document.body.style.background = "green";
+  }
+  else {
+    document.body.style.background = "blue";
+  }
+});
